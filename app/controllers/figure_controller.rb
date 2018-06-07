dde2dfaa0a12346f86091c316c9e3afb89018a51
github username <github email address>
2018-06-06 22:35:52 -0400

    Done.

--- app/controllers/figure_controller.rb
+++ app/controllers/figure_controller.rb
@@ -1,3 +1,12 @@
 class FiguresController < Sinatra::Base
 
+  get '/figures' do
+      @figures = Figure.all
+      erb :"figures/index"
+  end
+
+  get '/figures/new' do
+      erb :"figures/new"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,13 +2,17 @@ class LandmarksController < ApplicationController
 
 
   get '/landmarks' do
-
+      @landmarks = Landmark.all
+      @figures = Figure.all
       erb :"landmarks/index"
   end
-  
-  get '/landmarks/new' do
 
+  get '/landmarks/new' do
       erb :"landmarks/new"
   end
+
+  # post '/landmarks'
+  #
+  # end
 
 end
